47cb57f4cd47f706133a55f0ba55d05c13ff4351
Yasio <baszczok@yasio.pl>
2020-08-23 21:18:43 +0200

    precommit hook

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 [WIP]
-# CodersRailroad
+# CodersRailroad 🚂 
 
 This is a simple project made for Hactoberfest 2020. An easy entry to Pull Requests. Inspired by Twolio Open Pixel Art.
 
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>This is an about page 🚂</h1>
     <p>inspired by open pixel art by Twolio</p>
   </div>
 </template>
